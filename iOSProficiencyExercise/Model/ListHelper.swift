9d2d71b693d3f05fe0387d6f28b74e588fba3350
Nischal Hada <hadanischal@gmail.com>
2018-05-27 17:37:01 +1000

    code refactored

--- iOSProficiencyExercise/Model/ListHelper.swift
+++ iOSProficiencyExercise/Model/ListHelper.swift
@@ -10,8 +10,6 @@ import Foundation
 import UIKit
 
 public protocol dataSession {
-    func apiCall(completion: @escaping () -> Void)
-    func updateImageForCollectionViewCell(_ cell: UICollectionViewCell, inCollectionView collectionView: UICollectionView, imageURL: String, atIndexPath indexPath: IndexPath)
     func updateImageForTableViewCell(_ cell: UITableViewCell, inTableView tableView: UITableView, imageURL: String, atIndexPath indexPath: IndexPath)
 }
 
@@ -19,39 +17,8 @@ class ListHelper:dataSession{
     
     fileprivate let kLazyLoadCellImageViewTag = 1
     fileprivate let kLazyLoadPlaceholderImage = UIImage(named: "placeholder")!
-    typealias JSONDictionary = [String: Any]
-    var networkManager: NetworkManager { return NetworkManager() }
-    var parser: JSONParser { return JSONParser() }
-    var util: Util { return Util() }
     var imageManager: ImageManager { return ImageManager() }
-    
-    var responseResults = [ListModel]()
-    var title: String = ""
-    
-    func apiCall(completion: @escaping () -> Void){
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
-        let url = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
-        networkManager.request(URLString: url, parameters: nil){ results, errorMessage in
-            UIApplication.shared.isNetworkActivityIndicatorVisible = false
-            if let response = results {
-                print(results ?? "")
-                guard let title = response["title"]else {
-                    return
-                }
-                
-                self.title = self.util.filterNil(title as AnyObject) as! String
-                if let array = response["rows"] as? [AnyObject] {
-                    for properties in array {
-                        let currentData = ListModel(dictionary: properties as! [String : Any])
-                        self.responseResults.append(currentData)
-                    }
-                    completion()
-                }
-                
-            }
-        }
-    }
-    
+        
     func updateImageForCollectionViewCell(_ cell: UICollectionViewCell, inCollectionView collectionView: UICollectionView, imageURL: String, atIndexPath indexPath: IndexPath) {
         let imageView = cell.viewWithTag(kLazyLoadCellImageViewTag) as! UIImageView
         imageView.image = kLazyLoadPlaceholderImage
